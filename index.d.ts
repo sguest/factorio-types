f2f8f765909550563c080c6c4a62445703d457c1
sguest <spencer.b.guest@gmail.com>
2021-06-19 18:12:21 -0500

    Fix packaging

---
+++ index.d.ts
@@ -0,0 +1,7 @@
+/// <reference path="./dist/core.d.ts" />
+/// <reference path="./dist/global.d.ts" />
+/// <reference path="./dist/classes.d.ts" />
+/// <reference path="./dist/concepts.d.ts" />
+/// <reference path="./dist/defines.d.ts" />
+/// <reference path="./dist/events.d.ts" />
+/// <reference path="./dist/serpent.d.ts" />
--- src/index.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference path="./core.d.ts" />
-/// <reference path="./global.d.ts" />
-/// <reference path="./classes.d.ts" />
-/// <reference path="./concepts.d.ts" />
-/// <reference path="./defines.d.ts" />
-/// <reference path="./events.d.ts" />
-/// <reference path="./serpent.d.ts" />
