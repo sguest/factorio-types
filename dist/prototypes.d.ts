9a6b2ac1d7876fe19a3b1524adf4bfc839c60915
sguest <sguest@users.noreply.github.com>
2023-12-05 02:06:52 +0000

    Factorio API version 1.1.100

--- dist/prototypes.d.ts
+++ dist/prototypes.d.ts
@@ -2,7 +2,7 @@
 // Factorio API reference https://lua-api.factorio.com/latest/index.html
 // Generated from JSON source https://lua-api.factorio.com/latest/prototype-api.json
 // Definition source https://github.com/sguest/factorio-types
-// Factorio version 1.1.96
+// Factorio version 1.1.100
 // API version 4
 
 declare namespace prototype {
