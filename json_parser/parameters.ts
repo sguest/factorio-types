32b5b0cc2a864adcd5cbce0e98a27b787e2333b2
sguest <spencer.b.guest@gmail.com>
2025-03-05 21:56:50 -0600

    fix bad type interrupt_index

--- json_parser/parameters.ts
+++ json_parser/parameters.ts
@@ -4,5 +4,9 @@ import { questionToken } from './tsUtils';
 import { fixReservedName } from './format';
 
 export function parseParameter(parameter: Parameter) {
+    // Fix for some bad data
+    if(parameter.type === 'interrupt_index') {
+        [parameter.type, parameter.name] = [parameter.name, parameter.type];
+    }
     return ts.factory.createParameterDeclaration([], undefined, fixReservedName(parameter.name), parameter.optional ? questionToken : undefined, parseType(parameter.type), undefined)
 }
