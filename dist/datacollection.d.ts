68b9648bc8c043787ca1e8cde714c6bed9b130c5
sguest <sguest@users.noreply.github.com>
2025-09-24 02:39:16 +0000

    Factorio API version 2.0.68

--- dist/datacollection.d.ts
+++ dist/datacollection.d.ts
@@ -2,7 +2,7 @@
 // Factorio API reference https://lua-api.factorio.com/latest/index.html
 // Generated from JSON source https://lua-api.factorio.com/latest/prototype-api.json
 // Definition source https://github.com/sguest/factorio-types
-// Factorio version 2.0.67
+// Factorio version 2.0.68
 // API version 6
 
 declare namespace prototype {
